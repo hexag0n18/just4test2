3bea83ae1dbf9017db2f81e340c757b9b0f6e2ed
Josh Manic <jm6.abypro@gmail.com>
2022-01-17 11:03:39 -0500

    First commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "app.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [{ loader: "html-loader" }],
+      },
+      {
+        test: /\.(s?[ac]ss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+  devServer: {
+    watchFiles: [path.join(__dirname, "dist")],
+    compress: true,
+    port: 3001,
+  },
+};
